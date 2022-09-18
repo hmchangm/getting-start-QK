3133575dc645e775a97676b230e36008f8e404dd
hmchangm <dickgg@gmail.com>
2022-09-18 11:28:01 +0800

    Day11

--- src/main/kotlin/tw/brandy/ironman/FilmResource.kt
+++ src/main/kotlin/tw/brandy/ironman/FilmResource.kt
@@ -10,29 +10,37 @@ import javax.ws.rs.Produces
 import javax.ws.rs.core.MediaType
 
 @Path("/films")
-@Produces(MediaType.APPLICATION_JSON)
-@Consumes(MediaType.APPLICATION_JSON)
 class FilmResource(val filmService: FilmService) {
 
     @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
     suspend fun list() = filmService.getAllFilms()
 
     @GET
     @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
     suspend fun getById(id: Int) = filmService.getFilm(id)
 
     @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
     suspend fun add(film: Film) = filmService.save(film)
 
     @PUT
     @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
     suspend fun update(film: Film) = filmService.update(film)
 
     @DELETE
     @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
     suspend fun delete(id: Int) = filmService.delete(id)
 
     @GET
     @Path("/count")
+    @Produces(MediaType.APPLICATION_JSON)
     suspend fun count(): Long = filmService.getFilmCount()
 }
--- src/test/kotlin/tw/brandy/ironman/FilmResourceTest.kt
+++ src/test/kotlin/tw/brandy/ironman/FilmResourceTest.kt
@@ -1,23 +1,65 @@
 package tw.brandy.ironman
 
 import io.quarkus.test.junit.QuarkusTest
+import io.restassured.http.ContentType
 import io.restassured.module.kotlin.extensions.Given
 import io.restassured.module.kotlin.extensions.Then
 import io.restassured.module.kotlin.extensions.When
+import kotlinx.datetime.LocalDate
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 import org.hamcrest.CoreMatchers.equalTo
+import org.hamcrest.CoreMatchers.`is`
 import org.junit.jupiter.api.Test
 
 @QuarkusTest
 class FilmResourceTest {
 
     @Test
-    fun testHelloEndpoint() {
-        Given { log().all() }
-        When {
-            get("/hello")
+    fun `test list all`() {
+        Given {
+            contentType(ContentType.JSON)
+        } When {
+            get("/films")
         } Then {
             statusCode(200)
-            body(equalTo("Hello from RESTEasy Reactive"))
+            body("title", `is`(listOf("A New Hope", "The Empire Strikes Back", "Return Of The Jedi")))
+        }
+    }
+
+    @Test
+    fun `test add than update than delete`() {
+        Given {
+            contentType("application/json")
+            body(
+                Film("Spider Man", 100, "Sam Raimi", LocalDate.parse("2002-04-29"))
+                    .let { Json.encodeToString(it) }
+            )
+        } When {
+            post("/films")
+        } Then {
+            statusCode(200)
+            body("title", `is`("Spider Man"))
+            body("director", equalTo("Sam Raimi"))
+        }
+
+        Given {
+            contentType("application/json")
+            body(
+                Film("Spider Man", 100, "Nobody", LocalDate.parse("2002-04-29"))
+                    .let { Json.encodeToString(it) }
+            )
+        } When {
+            put("/films/100")
+        } Then {
+            statusCode(200)
+            body("director", equalTo("Nobody"))
+        }
+
+        When {
+            delete("/films/100")
+        } Then {
+            statusCode(204)
         }
     }
 }
