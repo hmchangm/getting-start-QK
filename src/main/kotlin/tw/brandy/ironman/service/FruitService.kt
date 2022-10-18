e055172081928309c24bb86c655134d37420f912
hmchangm <dickgg@gmail.com>
2022-10-18 16:05:24 +0800

    Use Quarkus Cache

--- src/main/kotlin/tw/brandy/ironman/service/FruitService.kt
+++ src/main/kotlin/tw/brandy/ironman/service/FruitService.kt
@@ -1,21 +1,16 @@
 package tw.brandy.ironman.service
 
 import arrow.core.Either
-import io.smallrye.mutiny.Uni
 import io.smallrye.mutiny.coroutines.awaitSuspending
 import org.eclipse.microprofile.rest.client.inject.RestClient
 import tw.brandy.ironman.FruitServiceCallError
-import tw.brandy.ironman.entity.FruityVice
 import javax.enterprise.context.ApplicationScoped
 
 @ApplicationScoped
 class FruitService(@RestClient val fruityViceService: FruityViceService) {
 
-    suspend fun findByNameCall(name: String): FruityVice =
+    suspend fun findByName(name: String) = Either.catch {
         fruityViceService.getFruitByName(name)
             .onFailure().retry().atMost(3).awaitSuspending()
-
-    suspend fun findByName(name: String) = Either.catch {
-        findByNameCall(name)
     }.mapLeft { FruitServiceCallError(it) }
 }
